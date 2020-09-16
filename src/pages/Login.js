0612a37b6385b5847270c6f5a0efdfcb55565d55
Nusrat Jahan <njnusrat53@gmail.com>
2020-09-16 23:25:39 +0600

    sign in page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from '../App';
 
 const Login = () => {
+	const [ loggedIn, setLoggedIn ] = useContext(UserContext);
+
 	return (
 		<div>
-			<h2>I am from login</h2>
+			<form>
+				<div className="form-group">
+					<label htmlFor="exampleInputEmail1">Email address</label>
+					<input type="email" className="form-control" aria-describedby="emailHelp" />
+					<small id="emailHelp" className="form-text text-muted">
+						We'll never share your email with anyone else.
+					</small>
+				</div>
+				<div className="form-group">
+					<label htmlFor="exampleInputPassword1">Password</label>
+					<input type="password" className="form-control" id="exampleInputPassword1" />
+				</div>
+				<button>Sign In</button>
+			</form>
 		</div>
 	);
 };
