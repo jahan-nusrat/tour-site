f5645c492ae50aab4fa1227e9f84d56a0156fce5
Nusrat Jahan <njnusrat53@gmail.com>
2020-09-16 23:15:11 +0600

    error detected

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -11,6 +11,15 @@ const Hero = () => {
 		return str.length > n ? str.substr(0, n - 1) + '....' : str;
 	}
 
+	const handleClick = (data) => {
+		setHero({
+			name   : data.name,
+			info   : data.info,
+			img    : data.img,
+			active : true
+		});
+	};
+
 	return (
 		<HeroStyle className="hero-container">
 			<div className="hero-info">
@@ -23,17 +32,7 @@ const Hero = () => {
 			<div className="hero-img">
 				{fakeData.map((data) => {
 					return (
-						<div
-							key={data.id}
-							className={data.title}
-							onClick={() =>
-								setHero({
-									name   : data.name,
-									info   : data.info,
-									img    : data.img,
-									active : true
-								})}
-						>
+						<div key={data.id} onClick={() => handleClick(data)}>
 							<img src={data.url} alt={data.name} />
 						</div>
 					);
